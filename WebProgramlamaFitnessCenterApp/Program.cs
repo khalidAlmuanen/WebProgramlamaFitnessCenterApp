973ff4ccdc9f0d88096ff098c0211e4778d85417
khalid almuanen <kly48170@GMAIL.COM>
2025-12-08 11:36:51 +0300

    chore: DbContext servisi Program.cs içine eklendi

--- WebProgramlamaFitnessCenterApp/Program.cs
+++ WebProgramlamaFitnessCenterApp/Program.cs
@@ -1,4 +1,13 @@
+using WebProgramlamaFitnessCenterApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
